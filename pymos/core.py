783782601ec479f53fe09f8c208756a7439efdcd
Abhishek Mishra <ideamonk@gmail.com>
2011-05-17 22:18:54 +0530

    added r-tree support, yet to add arguments for turning it on/off. added nearest query cache for both brute search and r-tree based search. removed cmp calls that wasted lot of time as seen in v0.6.3 profile. abstracted out rtree and normal colormap generators. resized images are now properly cached into image_cache. colormap is no more an ugly tuple based list. one would argue, why not a dictionary, but then it is required as we're finding out min euclidean dist and need to search the whole space. While using R-trees, its upto the user to have libspatialindex installed and setup, as of now. Got a 74s gain using these improvements in generating mosaics out of 1000+ thumbnails.

--- pymos/core.py
+++ pymos/core.py
@@ -24,6 +24,7 @@ except ImportError:
     import pickle
 
 USING_RTREE = False
+NEAREST_CACHE = {}
 
 try:
     import rtree
@@ -44,7 +45,7 @@ def get_average_rgb(image, filename):
 
     try:
         for i in imdata:
-            if cmp(type(i).__name__, 'int') == 0:
+            if type(i) == int:
                 red += i
                 green += i
                 blue += i
@@ -79,22 +80,84 @@ def get_linear_colormap(files):
         if not rgb:
             continue
         
+        colormap.append( ( rgb, eachfile ))
+        # caching of resized images done in image_cache now
+
+        file_count += 1
+        log.debug("%.1f %% done" % ((float(file_count)/total_files)*100))
+    return colormap
+
+
+def get_rtree_properties():
+    p = rtree.index.Property()
+    p.dimension = 3
+    p.dat_extension = 'data'
+    p.idx_extension = 'index'
+    return p
+    
+    
+def get_rtree_colormap(files, colormap_file):
+    log = logging.getLogger("PyMos")
+    
+    if os.path.isfile(colormap_file + ".index"):
+        os.remove(colormap_file + ".index")
+    if os.path.isfile(colormap_file + ".data"):
+        os.remove(colormap_file + ".data")
+        
+    colormap = rtree.index.Index(colormap_file, properties=get_rtree_properties())
+    # TODO: confirm if this is in-memory and has nothing to do with the 
+    # .colormap.data and .colormap.index files and can be painlessly pickled
+    
+    file_count = 0
+    total_files = len(files)
+    
+    for eachfile in files:
+        try:
+            temp = Image.open(eachfile)
+        except IOError as error:
+            log.debug("Error opening %s" % eachfile)
+            log.debug("IOError - %s" % error)
+            continue
+
+        rgb = get_average_rgb(temp, eachfile)
+        if not rgb:
+            continue
+            
+        r,g,b = rgb
         # append to colormap
-        colormap.append( ( rgb, eachfile, None ))
+        colormap.add( file_count, (r,g,b,r,g,b), eachfile)
         # ^^ new format for colormap, None replaced with resized images
         # when processing to cache already loaded images
 
         file_count += 1
         log.debug("%.1f %% done" % ((float(file_count)/total_files)*100))
     return colormap
-        
-def build_colormap(files):
+    
+    
+def build_colormap(files, colormap_file):
     ''' builds out and returns an average color to file location mapping '''
     if not USING_RTREE:
         return get_linear_colormap(files)
     else:
-        return get_rtree_colormap(files)
+        return get_rtree_colormap(files, colormap_file)
 
+
+def get_euclidean_match(source_color, colormap):
+    # euclidean distance, color, index in colormap
+    r_1, g_1, b_1 = source_color
+    match = (196608, (555, 555, 555), 0) # initially something out of range
+    for index, thumbs in zip (xrange(len(colormap)), colormap):
+        thumb_color = thumbs[0]
+        # calculate the euclidian distance between the two colors
+        r_2, g_2, b_2 = thumb_color
+
+        ecd_match = match[0]
+        ecd_found = ( (r_2 - r_1) ** 2 + (g_2 - g_1) ** 2 +
+                        (b_2-b_1) ** 2 )
+
+        if (ecd_found < ecd_match):
+            match = (ecd_found, thumb_color, index)
+    return colormap[match[2]][1]
 
 def build_mosaic(input_path, output_path, collection_path,
                     zoom = 20, thumb_size = 60, fuzz = 0, new_colormap=False):
@@ -109,17 +172,17 @@ def build_mosaic(input_path, output_path, collection_path,
 
     files = glob.glob(os.path.join(collection_path, '*.jpg'))
     colormap_file = os.path.join(collection_path, '.colormap')
-
+    NEAREST_CACHE = {}
+    
     if os.path.exists(colormap_file) and not new_colormap:
-        colormap = pickle.load(open(colormap_file))
+        if not USING_RTREE:
+            colormap = pickle.load(open(colormap_file))
+        else:
+            colormap = rtree.index.Index(colormap_file, properties=get_rtree_properties())
     else:
-        #try:
-        colormap = build_colormap(files)
-        pickle.dump(colormap, open(colormap_file, 'w'))
-        #except IOError:
-        #    log.info( "Error: Collection not found.")
-        #    sys.exit(1)
-    # this exception might cause it to break even at x% of colormap build
+        colormap = build_colormap(files, colormap_file)
+        if not USING_RTREE:
+            pickle.dump(colormap, open(colormap_file, 'w'))
 
     log.info("Color Index built")
 
@@ -136,67 +199,50 @@ def build_mosaic(input_path, output_path, collection_path,
 
     output = Image.new("RGB", (output_width, output_height),
                             (255,255,255))
-
+    image_cache = {}
     log.info("Generating Mosaic...")
+    
     # square mosaics as for now
     for s_x in xrange(0, output_width, thumb_size):
         for s_y in xrange(0, output_height, thumb_size):
             source_color = source_data[ (s_y/zoom) * source_width + s_x/zoom ]
+            is_bw = type(source_color) == int
             
             # we randomize source color for added fuziness
             if (fuzz!=0):
-                if cmp(type(source_color).__name__, 'int') == 0:
+                if is_bw:
                     source_color = random.randint(-fuzz, fuzz) + source_color
                 else:
                     source_color = tuple(s_x + random.randint(-fuzz, fuzz)
                                         for s_x in source_color)
                                         
-            if cmp(type(source_color).__name__, 'int') == 0:
-                r_1 = g_1 = b_1 = source_color
+            if is_bw:
+                source_color = (source_color, source_color, source_color)
+                
+            if not USING_RTREE:
+                match = get_euclidean_match(source_color, colormap)
             else:
-                r_1, g_1, b_1 = source_color
+                if source_color in NEAREST_CACHE:
+                    match = NEAREST_CACHE[source_color]
+                else:
+                    match = NEAREST_CACHE[source_color] = \
+                            colormap.nearest(source_color, 1, objects=True).next().object
             
-            # euclidean distance, color, index in colormap
-            match = (196608, (555, 555, 555), 0)# initially something out of range
-
-            for index, thumbs in zip (xrange(len(colormap)), colormap):
-                thumb_color = thumbs[0]
-                # calculate the euclidian distance between the two colors
-                r_2, g_2, b_2 = thumb_color
-
-                ecd_match = match[0]
-                ecd_found = ( (r_2 - r_1) ** 2 + (g_2 - g_1) ** 2 +
-                                (b_2-b_1) ** 2 )
-                                
-                if (ecd_found < ecd_match):
-                    match = (ecd_found, thumb_color, index)
-
-            try:
-                if (colormap[match[2]][2] == None):   # has not been resized yet
-                    colormap[match[2]] = (colormap[match[2]][0],
-                    colormap[match[2]][1], Image.open(colormap[match[2]][1]))
-
+            if not (match in image_cache):
+                image_cache[match] = Image.open(match)
                 ### new maxfill method
-                tsize = colormap[match[2]][2].size
-
+                tsize = image_cache[match].size
                 # taller image -> fille width to complete square
-                tsize =  (
-                        thumb_size,
-                        int( round((float(tsize[1])/tsize[0]) * thumb_size))
-                 )
+                tsize =  (thumb_size,
+                        int( round((float(tsize[1])/tsize[0]) * thumb_size)))
 
                 if ( tsize[0] > tsize[1]):
                     # wider image -> fill height of thumb_sizexthumb_size square
-                    tsize =  (
-                        int( round((float(tsize[0])/tsize[1]) * thumb_size )),
-                        thumb_size
-                    )
-
-                output.paste (colormap[match[2]][2].resize (tsize), (s_x, s_y))
-
-            except ValueError:
-                log.debug ("No match for " + source_color)
-
+                    tsize =  (int( round((float(tsize[0])/tsize[1]) * thumb_size )),
+                        thumb_size)
+                image_cache[match] = image_cache[match].resize(tsize)
+                
+            output.paste (image_cache[match], (s_x, s_y))
         log.debug("%.1f %% done" % ((float(s_x)/output_width) * 100))
 
 
